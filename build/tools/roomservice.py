763dee9010b274a529038ac3a6851b233e2847a8
Etherwvlf <etherwvlf@pm.me>
2018-04-19 11:42:25 +0300

    Update roomservice.py

--- build/tools/roomservice.py
+++ build/tools/roomservice.py
@@ -151,9 +151,9 @@ def is_in_manifest(projectpath):
         if localpath.get("path") == projectpath:
             return True
 
-    # ... and don't forget the lineage snippet
+    # ... and don't forget the hellfire snippet
     try:
-        lm = ElementTree.parse(".repo/manifests/snippets/cm.xml")
+        lm = ElementTree.parse(".repo/manifests/snippets/hellfire.xml")
         lm = lm.getroot()
     except:
         lm = ElementTree.Element("manifest")
